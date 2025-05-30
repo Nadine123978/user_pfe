fc8770d73f75b73ce9ddd8891082f787e14d77f2
Nadine <nadinemead11@gmail.com>
2025-05-30 11:59:09 +0300

    Add AdventureCard component with design and Book Now button

--- src/pages/user/booking.jsx
+++ src/pages/user/booking.jsx
@@ -1,15 +1,15 @@
 // booking.jsx
 import React from "react";
 import { useParams } from "react-router-dom";
-import HeaderBooking from "../components/Headerbooking";
-import EventBanner from "../components/EventBanner";
-import EventDetails from "../components/EventDetails";
-import TicketSectionSelector from "../components/TicketSectionSelector";
-import SeatingMap from "../components/SeatingMap";
-import PriceLegend from "../components/PriceLegend";
-import OrganizerInfo from "../components/OrganizerInfo";
-import VenueMap from "../components/VenueMap";
-import PaymentFooter from "../components/PaymentFooter";
+import HeaderBooking from "../../components/user/Headerbooking";
+import EventBanner from "../../components/user/EventBanner";
+import EventDetails from "../../components/user/EventDetails";
+import TicketSectionSelector from "../../components/user/TicketSectionSelector";
+import SeatingMap from "../../components/user/SeatingMap";
+import PriceLegend from "../../components/user/PriceLegend";
+import OrganizerInfo from "../../components/user/OrganizerInfo";
+import VenueMap from "../../components/user/VenueMap";
+import PaymentFooter from "../../components/user/PaymentFooter";
 
 const Booking = () => {
   const { id } = useParams(); // ← بتجيب 43 من /booking/43
